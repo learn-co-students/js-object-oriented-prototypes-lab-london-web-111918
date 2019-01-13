1b7d7c4e906683c9efc05b9933ceebe1c35330e6
Andy <andypurbrick@gmail.com>
2019-01-13 14:58:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
-function BoardMember(name, homeState, training){
-  this.name = name
-  this.homeState = homeState
-  this.training = training
-}
+class BoardMember {
+  constructor(name, homeState, training) {
+    this.name = name
+    this.homeState = homeState
+    this.training = training
+  }
 
-BoardMember.prototype.veto = function(){
-  return 'No, I must disagree'
-}
+  veto() {
+    return 'No, I must disagree'
+  }
 
-BoardMember.prototype.approve = function(){
-  return 'You can do that!'
-}
+  approve() {
+    return 'You can do that!'
+  }
 
-BoardMember.prototype.doCharity = function(){
-    return "I like to help people."
-}
+  doCharity() {
+    return 'I like to help people.'
+  }
 
-BoardMember.prototype.releasePressStatement = function(){
-    return "You will see great things from Scuber."
-}
+  releasePressStatement() {
+    return 'You will see great things from Scuber.'
+  }
 
-BoardMember.prototype.sayHi = function(){
+  sayHi() {
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  }
+}
+
+class Ceo extends BoardMember {
+  hireEmployee() {
+    return 'Welcome aboard!'
+  }
 }
